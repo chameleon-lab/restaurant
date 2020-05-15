54b19fdc1a1884bf94e0c4355a9e0aa27ae42b5d
o.tymchuk <o.tymchuk@krok.edu.ua>
2020-05-15 15:25:29 +0300

    2 commit

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -145,9 +145,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = "/static/"
 
-STATICFILES_DIRS = [
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-]
+)
